eafcdb39c47a4fa9cc152927d85f6a63ce0f234a
Orlando Arnosa <oarnosa@gmail.com>
2024-05-10 10:16:38 -0400

    Update copyright notice

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,8 @@ const Footer = () => (
   <footer className="text-white bg-theme-primary">
     <div className="container">
       <p className="flex items-center justify-center text-sm text-center h-14">
-        Copyright &copy; 2023 | Simplified Web.Dev | All Rights Reserved
+        Copyright &copy; {new Date().getFullYear()} | Simplified Web.Dev | All
+        Rights Reserved
       </p>
     </div>
   </footer>
